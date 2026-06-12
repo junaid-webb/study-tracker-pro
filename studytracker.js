e6707d041a8e1dc12a1a083e5b999cb5b98d030d
junaid-webb <mohamadjunaidrciet@gmail.com>
2026-06-12 17:01:20 +0530

    Implement task completion and deletion

--- studytracker.js
+++ studytracker.js
@@ -1,4 +1,4 @@
-function addTask(){
+function addTask() {
 
     const taskInput =
     document.getElementById("taskInput");
@@ -6,16 +6,38 @@ function addTask(){
     const taskList =
     document.getElementById("taskList");
 
-    if(taskInput.value.trim() === "")
+    if (taskInput.value.trim() === "")
         return;
 
     const li =
     document.createElement("li");
 
-    li.textContent =
-    taskInput.value;
+    li.innerHTML = `
+        <span>${taskInput.value}</span>
+
+        <button onclick="toggleTask(this)">
+            Complete
+        </button>
+
+        <button onclick="deleteTask(this)">
+            Delete
+        </button>
+    `;
 
     taskList.appendChild(li);
 
     taskInput.value = "";
+}
+
+function toggleTask(button) {
+
+    const task =
+    button.parentElement.querySelector("span");
+
+    task.classList.toggle("completed");
+}
+
+function deleteTask(button) {
+
+    button.parentElement.remove();
 }
